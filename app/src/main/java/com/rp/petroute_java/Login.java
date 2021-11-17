1bda39ea84b96665d362a577aef008e4fca7e97c
Mauricio Santos <maosantos@outlook.com>
2021-11-16 22:04:05 -0500

    QUE= vista Forms PARA= Consultar ruta COMO= Copia de Login y adaptacion QUE FALTA= En esta vista falta el Mapa (desarrollo posterior) Strings Hardcoded = cambiar
    QUE= vista Login
    PARA= ingresar con contrasena y pasar a vista Forms
    COMO= Metodo Ingresar
    QUE FALTA= Que no este quemado y conecte a DB

--- app/src/main/java/com/rp/petroute_java/Login.java
+++ app/src/main/java/com/rp/petroute_java/Login.java
@@ -2,15 +2,36 @@ package com.rp.petroute_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+
+    private EditText nameUserInput, passUserInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        nameUserInput = findViewById(R.id.nameUserInput);
+        passUserInput = findViewById(R.id.passUserInput);
 
+    }
+
+    public void Ingresar (View view){
+        String userId = nameUserInput.getText().toString();
+        String userPass = passUserInput.getText().toString();
+
+        if (userId.equals("admin") && userPass.equals("12345")){
+            Intent i = new Intent(this, Forms.class);
+            i.putExtra("Nombre", userId);
+            startActivity(i);
+        }else{
+            Toast message = Toast.makeText(this, "Por favor verifica tu usuario y contraseña", Toast.LENGTH_LONG);
+        }
     }
 }
