e6f6b50957d63de42a82c0a6606fe0f56b39ea72
Mauricio Santos <maosantos@outlook.com>
2021-11-16 22:00:49 -0500

    QUE= vista Forms PARA= Consultar ruta COMO= Copia de Login y adaptacion QUE FALTA= En esta vista falta el Mapa (desarrollo posterior) Strings Hardcoded = cambiar

---
+++ app/src/main/java/com/rp/petroute_java/Forms.java
@@ -0,0 +1,14 @@
+package com.rp.petroute_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Forms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forms);
+    }
+}
