dacbb835cece04f609ce7b6ba9b2b585e825a92e
Raul Polania <raulpolania@gmail.com>
2021-11-18 00:16:57 -0500

    Funcionalidades botones en login y register.
    Pantalla de bienvenida con mascotas en ruta.
    
    Falta añadir boton para ingresar una nueva mascota (o menu para añadir funcionalidad) y registrar en bases de datos.

--- app/src/main/java/com/rp/petroute_java/Login.java
+++ app/src/main/java/com/rp/petroute_java/Login.java
@@ -6,7 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class Login extends AppCompatActivity {
 
@@ -17,21 +20,49 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        nameUserInput = findViewById(R.id.nameUserInput);
-        passUserInput = findViewById(R.id.passUserInput);
+        TextView name = findViewById(R.id.nameUserInput);
+        TextView pass = findViewById(R.id.passUserInput);
+
+        MaterialButton loginBut = findViewById(R.id.btnlogin);
+        MaterialButton registerBut = findViewById(R.id.btnregister);
+
+        loginBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String userId = name.getText().toString();
+                String userPass = pass.getText().toString();
+
+                if (userId.equals("admin") && userPass.equals("12345")){
+                    Toast.makeText(Login.this, "Bienvenido a PetRoute", Toast.LENGTH_LONG).show();
+                    Intent i = new Intent(Login.this, Mascotas.class);
+                    i.putExtra("Nombre", userId);
+                    startActivity(i);
+                }else{
+                    Toast.makeText(Login.this, "Ops! no estas registrado. Ingresa en el botón Registrate o verifica tus credenciales", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+        registerBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Registro(v,name,pass);
+            }
+        });
+
 
     }
 
-    public void Ingresar (View view){
-        String userId = nameUserInput.getText().toString();
-        String userPass = passUserInput.getText().toString();
+    public void Registro (View view, TextView name, TextView pass){
+        String userId = name.getText().toString();
+        String userPass = pass.getText().toString();
 
-        if (userId.equals("admin") && userPass.equals("12345")){
-            Intent i = new Intent(this, Forms.class);
+            Intent i = new Intent(Login.this, register.class);
             i.putExtra("Nombre", userId);
             startActivity(i);
-        }else{
-            Toast message = Toast.makeText(this, "Por favor verifica tu usuario y contraseña", Toast.LENGTH_LONG);
-        }
+            Toast.makeText(Login.this, "Llena tus datos", Toast.LENGTH_SHORT).show();
+
     }
 }
