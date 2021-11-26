00b1096a6aeb65f544e01317e820d88f246ca088
Mauricio Santos <maosantos@outlook.com>
2021-11-25 23:28:03 -0500

    Login y Dialog
    Login actualizado
    Clase Dialog (falta codigo)

---
+++ app/src/main/java/com/rp/petroute_java/Dialog.java
@@ -0,0 +1,46 @@
+package com.rp.petroute_java;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import org.w3c.dom.Text;
+
+
+public class Dialog {
+    Dialog dialog;
+    public boolean answerYes;
+
+    public Dialog(@Nullable Context context, String titulo, String text, View.OnClickListener onclicOK) {
+        dialog = new Dialog(context);
+        dialog.setContentView(R.layout.custom_dialog);
+        TextView textTitle = dialog.findViewById(R.id.textView);
+        TextView textDialog =  dialog.findViewById(R.id.textView2);
+        textTitle.setText(titulo);
+        textDialog.setText(text);
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.setCancelable(false);
+
+        Button cancel = dialog.findViewById(R.id.btn_cancel);
+        Button okay = dialog.findViewById(R.id.btn_okay);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        okay.setOnClickListener(onclicOK);
+        dialog.show();
+    }
+}
+
--- app/src/main/java/com/rp/petroute_java/Login.java
+++ app/src/main/java/com/rp/petroute_java/Login.java
@@ -4,65 +4,76 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
+import com.rp.petroute_java.persistencia.DbUsers;
 
 public class Login extends AppCompatActivity {
 
-    private EditText nameUserInput, passUserInput;
+    EditText username, password;
+    Button loginbtn, btnRegis; // Declaramos de forma global nuestra variable Button
+    DbUsers DB;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        getSupportActionBar().hide();
+
         TextView name = findViewById(R.id.nameUserInput);
         TextView pass = findViewById(R.id.passUserInput);
 
+        username = findViewById(R.id.btnlogin);
+        password = findViewById(R.id.btnregister);
+
+// Seguro que estos botones son del login??? o son del Register???
         MaterialButton loginBut = findViewById(R.id.btnlogin);
         MaterialButton registerBut = findViewById(R.id.btnregister);
+        DB = new DbUsers(this);
 
         loginBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String user = name.getText().toString();
+                String pass = pass.getText().toString();
 
-                String userId = name.getText().toString();
-                String userPass = pass.getText().toString();
-
-                if (userId.equals("admin") && userPass.equals("12345")){
-                    Toast.makeText(Login.this, "Bienvenido a PetRoute", Toast.LENGTH_LONG).show();
-                    Intent i = new Intent(Login.this, Mascotas.class);
-                    i.putExtra("Nombre", userId);
-                    startActivity(i);
-                }else{
-                    Toast.makeText(Login.this, "Ops! no estas registrado. Ingresa en el botón Registrate o verifica tus credenciales", Toast.LENGTH_LONG).show();
+                if (TextUtils.isEmpty(user) || TextUtils.isEmpty(pass))
+                    Toast.makeText(Login.this, "Todos los espacios son requeridos", Toast.LENGTH_SHORT).show();
+                else {
+                    Boolean checkuserpass = DB.checkPassword(user, pass);
+                    if (checkuserpass == true) {
+                        // Toast.makeText(MainActivity.this, "Login correcto", Toast.LENGTH_SHORT).show();
+                        Dialog d = new Dialog(Login.this,"Aqui va el titulo","Hola gente", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+//                                Intent intent = new Intent(getApplicationContext(), CardsItems.class);
+//                                startActivity(intent);
+                            }
+                        });
+                    } else {
+                        Toast.makeText(Login.this, "Login incorrecto", Toast.LENGTH_SHORT).show();
+                    }
                 }
-
             }
+
         });
 
-        registerBut.setOnClickListener(new View.OnClickListener() {
+
+        btnRegis.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Registro(v,name,pass);
+            public void onClick(View view) {
+                // Cambiamos de layout/Vista
+                Intent intent = new Intent(getApplicationContext(), Register.class);
+                startActivity(intent);
             }
         });
-
-
-    }
-
-    public void Registro (View view, TextView name, TextView pass){
-        String userId = name.getText().toString();
-        String userPass = pass.getText().toString();
-
-            Intent i = new Intent(Login.this, Register.class);
-            i.putExtra("Nombre", userId);
-            startActivity(i);
-            Toast.makeText(Login.this, "Llena tus datos", Toast.LENGTH_SHORT).show();
-
     }
 }
